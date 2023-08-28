d45429996d7167be24fa539532c92d561aefc4d0
Sanderson Sa <costadesa@gmal.com>
2023-08-28 17:48:30 -0300

    changes

--- src/main/java/xyz/sandersonsa/quarkuscamelkafkaconsumer/route/ConsumerRoute.java
+++ src/main/java/xyz/sandersonsa/quarkuscamelkafkaconsumer/route/ConsumerRoute.java
@@ -13,7 +13,17 @@ public class ConsumerRoute extends RouteBuilder {
     public void configure() throws Exception {
 
         // kafka consumer
-        from("kafka:{{kafka.topic.name}}")
+        // from("kafka:{{kafka.topic.name}}")
+
+        from("kafka:{{kafka.topic.name}}?brokers={{app.kafka.brokers}}"
+                // Setup the topic and broker address
+                + "&groupId={{app.consumer.groupid}}"
+                // The consumer processor group ID
+                + "&autoOffsetReset=earliest"
+                // Ask to start from the beginning if we have unknown offset
+                // + "&offsetRepository=#offsetRepo"
+                )
+                // Keep the offsets in the previously configured repository
                 .routeId("FromKafka2Seda")
                 .log("Received : \"${body}\"")
                 .to("seda:kafka-messages");
